8e9b5bfcd2380b123857bdab8c5324549b33ae5d
simesh19 <simeshjunghare@gmail.com>
2023-06-14 22:07:38 +0530

    commit

---
+++ iPortfolio/forms/contact.php
@@ -0,0 +1,41 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'simeshjunghare@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  $contact->smtp = array(
+    'host' => 'smtp.mailgun.org',
+    'username' => 'postmaster@sandbox10ae95f6d2f943788ccdfcc90a627780.mailgun.org',
+    'password' => '75896076063d8d1183edc945638615b8-5d9bd83c-475b241d',
+    'port' => '587'
+  );
+
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
